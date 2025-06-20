1832ba62c156012bde0b3c22df149defacb4dcf5
Wata0912 <241211@st.yoshida-g.ac.jp>
2025-06-20 11:39:44 +0900

    #2 カードの値から役の判定をできるように実装

--- TrumpGame/Program.cs
+++ TrumpGame/Program.cs
@@ -38,6 +38,44 @@ namespace TrumpGame
 
                 }
 
+            //ペアの数
+            int count = 0;
+            //比較元の設定
+            for (int j = 0; j < 4; j++)
+            {
+                //比較先の設定、残り枚数分繰り返す
+                for (int i = j + 1; i < 4; i++)
+                {
+                    //比較元と比較先の値が等しいときCountを+する
+                    if ((cards[i] & 0xf) == (cards[j] & 0xf))
+                    {
+                        //ペアの数をカウントする
+                        count++;
+                    }
+
+                }
+            }
+
+            //役の判定と出力
+            switch (count)
+            {
+                case 0:
+                    Console.WriteLine("ハイカード");
+                    break;
+                case 1:
+                    Console.WriteLine("ワンペア!");
+                    break;
+                case 2:
+                    Console.WriteLine("ツーペア!!");
+                    break;
+                case 3:
+                    Console.WriteLine("スリーカード!!!");
+                    break;
+                case 6:
+                    Console.WriteLine("フォーカード!!!!");
+                    break;
+            }
+
             Console.WriteLine("Enterを押してください");
             Console.ReadLine();
             
